50623a772a7a3f76ea66c72259c3732287c581f4
brennanananan <brennanlarson@icloud.com>
2017-06-14 15:31:46 -0700

    added times to meeting

--- src/main/java/com/hackday/farmtotable/skilldata/MeetingData.java
+++ src/main/java/com/hackday/farmtotable/skilldata/MeetingData.java
@@ -1,5 +1,31 @@
 package com.hackday.farmtotable.skilldata;
 
+import java.time.LocalDateTime;
+
 public class MeetingData extends SkillData {
 
+	private LocalDateTime start;
+	private LocalDateTime end;
+	
+	public LocalDateTime getStart() {
+		return start;
+	}
+	
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+	
+	public LocalDateTime getEnd() {
+		return end;
+	}
+	
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+
+	@Override
+	public String toString() {
+		return "MeetingData [start=" + start + ", end=" + end + "]";
+	}
+	
 }
