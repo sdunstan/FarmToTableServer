2df1cb2cc8d014dc7bbefdb131b083d156cda021
brennanananan <brennanlarson@icloud.com>
2017-06-14 14:57:35 -0700

    updated training

--- src/main/java/com/hackday/farmtotable/intent/IntentDeterminator.java
+++ src/main/java/com/hackday/farmtotable/intent/IntentDeterminator.java
@@ -3,15 +3,23 @@ package com.hackday.farmtotable.intent;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import com.hackday.farmtotable.enums.SkillType;
-import com.hackday.farmtotable.skilldata.SkillDataCreatorFactory;
 
 @Component
 public class IntentDeterminator {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(IntentDeterminator.class);
+	
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	public IntentDeterminator(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 	
 	public SkillType determineIntent(Object message) {
 		String data = (String) message;
